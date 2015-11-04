ba76aea27c292b016d112a5448343a2f5bc60e2b
Craig Carlson <cmcarlson94@gmail.com>
2015-11-03 20:39:53 -0500

    Add code to AddHero and HeroStatOptions view controllers allowing display of tables and data

--- HeroRosterApp/HeroStatOptionsViewController.swift
+++ HeroRosterApp/HeroStatOptionsViewController.swift
@@ -8,28 +8,68 @@
 
 import UIKit
 
-class HeroStatOptionsViewController: UIViewController, UINavigationBarDelegate {
+class HeroStatOptionsViewController: UIViewController, UINavigationBarDelegate, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var navigationBarTitle: UINavigationItem!
+    
+    
+    var navBarTitle = String()
+    var chosenStat = Int()
+    var lastSelectedRow: NSIndexPath? = nil
+    var statFromPreviousView = String()
+    var heroStatsArrayToDisplay = [String]()
+    var detailedStatFromStartView = String()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         navigationBarTitle.title = navBarTitle
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroStatsArrayToDisplay.count
+    }
+    
+    @IBAction func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // This allows the checkmark to disappear from the current cell when a new cell is touched.
+        if let last = lastSelectedRow {
+            let oldSelectedCell = tableView.cellForRowAtIndexPath(last)
+            oldSelectedCell!.accessoryType = .None
+        }
+        
+        // This will place a checkmark on the cell which is touched.
+        lastSelectedRow = indexPath
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell!.accessoryType = .Checkmark
+        
+        chosenStat = indexPath.row
+        cell!.textLabel?.text = heroStatsArrayToDisplay[chosenStat]
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("optionCell", forIndexPath: indexPath)
+        cell.textLabel!.text = heroStatsArrayToDisplay[indexPath.row]
+        
+        // When the table loads, this will put a checkmark on the cell which was displayed on the previous view.
+        
+        if cell.textLabel!.text! == statFromPreviousView {
+            cell.accessoryType = .Checkmark
+            lastSelectedRow = indexPath
+        }
+        return cell
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+
 
 }
